d2c4ac7f904ee9fc99d4ae02bb514bef04853ec1
Logan <yitiansong4@gmail.com>
2025-05-27 12:23:33 +0800

    init

---
+++ src/main/java/org/example/demokafka/config/MyBean.java
@@ -0,0 +1,30 @@
+package org.example.demokafka.config;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyBean {
+
+	private final KafkaTemplate<String, String> kafkaTemplate;
+
+	public MyBean(KafkaTemplate<String, String> kafkaTemplate) {
+		this.kafkaTemplate = kafkaTemplate;
+	}
+
+	public void someMethod() {
+		this.kafkaTemplate.send("someTopic", "Hello");
+	}
+	int count = 0;
+
+	@KafkaListener(topics = "someTopic")
+	public void processMessage(String content) {
+		System.out.println("content = " + content);
+		if (count++ < 1000) {
+			kafkaTemplate.send("someTopic", content);
+		}
+
+	}
+
+}
